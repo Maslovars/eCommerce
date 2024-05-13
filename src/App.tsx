5ee5e433af9f97a9ce803b0ce6af23c54b8da7c0
Maslovars <maslovars@gmail.com>
2024-05-13 18:12:28 +0300

    feat: add registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { SignUp, Main, EmptyPage } from './pages';
+import { SignUp, Main, EmptyPage, RegistrationPage } from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/signIn" element={<SignUp />} />
+      <Route path="/signUp" element={<RegistrationPage />} />
       <Route path="/" element={<Main />} />
       <Route path="*" element={<EmptyPage />} />
     </Routes>
