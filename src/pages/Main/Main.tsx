306a7fae63e7ee935f5ef12c3c298e78c664de34
Maslovars <maslovars@gmail.com>
2024-05-30 17:35:46 +0300

    feat: add jest

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,26 +1,28 @@
 import { Header, ItemCard, Footer } from '../../components';
 import { Items } from '../../types.ts';
 import { useGetItems } from '../../hooks/';
-import CardPreview from '../../components/CardPreview.tsx';
-
+import { Loading } from '../../components/Loading.tsx';
 export const Main = () => {
   const items: Items[] = useGetItems();
   return (
     <div className="h-screen flex-col flex justify-between">
       <Header />
       <div>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 px-20">
-          {items?.map((item) => (
-            <ItemCard
-              key={item.id}
-              price={item.price}
-              imageSrc={item.image}
-              description={item.description}
-              title={item.title}
-            />
-          ))}
-          <CardPreview />
-        </div>
+        {items.length > 0 ? (
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 px-20">
+            {items?.map((item) => (
+              <ItemCard
+                key={item.id}
+                price={item.price}
+                imageSrc={item.image}
+                description={item.description}
+                title={item.title}
+              />
+            ))}
+          </div>
+        ) : (
+          <Loading />
+        )}
       </div>
       <Footer />
     </div>
